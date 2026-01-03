9980c69283c449dc47d5df07064b771a102d9727
GoldenPomelo <bigpear0201@gmail.com>
2026-01-03 10:18:06 +0800

    [UPDATE] glibc 228

--- build_wheels.sh
+++ build_wheels.sh
@@ -34,8 +34,9 @@ echo "Building wheels..."
 # but with abi3 it might pick one and build a generic wheel.
 maturin build --release $USE_ZIG \
     --strip \
-    --target x86_64-unknown-linux-gnu \
-    --out dist
+    --target x86_64-unknown-linux-gnu.2.28 \
+    --out dist \
+    --compatibility manylinux_2_28
 
 echo "Build complete. Wheels are in dist/"
 ls -lh dist/
